b3ede21bcd34ae527e9e66fb366601b8d6fe269c
Антонина Пимонова <ae.pimonova@gmail.com>
2022-09-17 20:08:18 +0300

    Added menu output

--- Pimonova_labs_plusi.cpp
+++ Pimonova_labs_plusi.cpp
@@ -4,11 +4,12 @@
 #include <iostream>
 using namespace std;
 
-enum {exit, addPipe, addStation, viewObjects, editPipe, editStation, save, download};
+enum menu { exitMenu, addPipe, addStation, viewObjects, editPipe, editStation, save, download };
 
 int main()
 {
-
+	cout << "Welcome to the main menu \nUse numbers to navigate:\n";
+	cout << "Exit:0 \nAdd pipe:1 \nAdd station:2 \nView objects:3 \nEdit pipe:4 \nSave:5 \nDownload:6 \n";
 }
 
 
