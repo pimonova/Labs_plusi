78a756e005ebdf68fc4896df9f75aa4054e5f534
Антонина Пимонова <ae.pimonova@gmail.com>
2022-09-24 00:48:25 +0300

    Add a function to input an unsighned int

--- Pimonova_labs_plusi.cpp
+++ Pimonova_labs_plusi.cpp
@@ -2,14 +2,52 @@
 //
 
 #include <iostream>
+#include <limits>
 using namespace std;
 
 enum menu { exitMenu, addPipe, addStation, viewObjects, editPipe, editStation, save, download };
+
+//functions
+
+void skipToInt()
+{
+    if (cin.fail())
+    {
+        cin.clear();
+        for (uint8_t ch; cin >> ch;)
+        { 
+            if (isdigit(ch) || ch == '-')
+            {
+                cin.unget(); 
+                return;
+            }
+        }
+    }
+    cout << ("No input");
+}
+
+uint32_t getUInt() // Ввод беззнакового инта
+{
+    uint32_t x;
+    while (true)
+    {
+        if (cin >> x) return x;
+        cout << "Try again:\n";
+        skipToInt();
+    }
+}
+
 
 int main()
 {
 	cout << "Welcome to the main menu \nUse numbers to navigate:\n";
 	cout << "Exit:0 \nAdd pipe:1 \nAdd station:2 \nView objects:3 \nEdit pipe:4 \nSave:5 \nDownload:6 \n";
+
+    cout << "Enter an operation: ";
+    uint32_t operation = getUInt();
+    cout << "OK";
+
+    return 0;
 }
 
 
