2b0ed46d6256c0ed247fdcb5833649d910af4be8
Антонина Пимонова <ae.pimonova@gmail.com>
2022-09-25 00:51:28 +0300

    Add operation input range check

--- Pimonova_labs_plusi.cpp
+++ Pimonova_labs_plusi.cpp
@@ -48,16 +48,15 @@ double_t getDouble() // Ввод вещественного числа
     }
 }
 
-uint32_t getInRange() // Ввод беззнакового инта
+uint32_t getInRange(uint8_t x1,uint8_t x2) // Ввод числа в диапазоне
 {
     while (true)
     {
         uint32_t x = getUInt();
-        if (0<=x && x<=100) return x;
+        if (x1<=x && x<=x2) return x;
         cout << "Try again:\n";
     }
 }
-
 
 int main()
 {
@@ -65,7 +64,7 @@ int main()
 	cout << "Exit:0 \nAdd pipe:1 \nAdd station:2 \nView objects:3 \nEdit pipe:4 \nSave:5 \nDownload:6 \n";
 
     cout << "Enter an operation: ";
-    uint32_t operation = getUInt();
+    uint32_t operation = getInRange(0,6);
     cout << "OK \n";
 
     cout << "Double: ";
@@ -73,7 +72,7 @@ int main()
     cout << "OK \n";
 
     cout << "In diapason 0-100: \n";
-    uint32_t chislzero = getInRange();
+    uint32_t chislzero = getInRange(0,100);
     cout << "OK \n";
 
     return 0;
